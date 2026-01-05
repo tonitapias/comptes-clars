e906a9e1bfb3e06da4b386d389fe7cdc82138a74
tonitapias <tonitapias@gmail.com>
2026-01-05 01:12:58 +0100

    Reparant soroll de Cònsola

--- vite.config.js
+++ vite.config.js
@@ -61,7 +61,7 @@ export default defineConfig({
   },
   server: {
     headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
+      'Cross-Origin-Opener-Policy': 'unsafe-none',
       'Cross-Origin-Embedder-Policy': 'unsafe-none',
     },
   },
